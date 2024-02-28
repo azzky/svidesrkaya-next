ce2d1bc69046cfe2d06f9a5de6a42bf81a4b3fbb
nkulikov-hot <nkulikov@helenoftroy.com>
2024-02-28 12:56:33 +0100

    fix for missing image in ru locale

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import Layout from '@/components/layout/layout';
 import * as classes from '@/styles/about.module.scss';
 
 const About = (props) => {
-    const { data } = props;
+    const { data, image } = props;
 
     return (
         <Layout>
@@ -19,7 +19,7 @@ const About = (props) => {
             isPage/> */}
             <div className={classes.root}>
                 <div className={classes.content}>
-                    <Image src={data.image.fields.file.url}
+                    <Image src={image.fields.file.url}
                         width={500}
                         height={500}
                         quality={95}
@@ -42,13 +42,18 @@ const About = (props) => {
 export default injectIntl(About);
 
 export const getStaticProps = async ({ locale }) => {
-    const res = await client.getEntries({
+    const image = await client.getEntries({
         content_type: 'about'
+    });
+    const res = await client.getEntries({
+        content_type: 'about',
+        locale: locale === 'en' ? 'en-US' : 'ru'
     });
 
     return {
         props: {
             data: res.items[0].fields,
+            image: image.items[0].fields.image,
             revalidate: 70,
             locale
         }
