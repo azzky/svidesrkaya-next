c0770e1746096bc16cba2ccad9b67e34959a0baf
nkulikov-hot <nkulikov@helenoftroy.com>
2024-03-22 14:43:40 +0100

    code fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { IntlProvider } from 'react-intl';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import en from '../lang/en.json';
 import ru from '../lang/ru.json';
@@ -15,6 +16,11 @@ export default function App({ Component, pageProps }) {
     const { locale } = useRouter();
     return (
         <IntlProvider locale={locale} messages={messages[locale]}>
+            <Head>
+                <meta name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=no"
+                />
+            </Head>
             <Component {...pageProps} dir="ltr"/>
         </IntlProvider>
     );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,9 +7,6 @@ export default function Document() {
                 <link rel="manifest" href="/manifest.json"/>
                 <link rel="manifest" href="/site.webmanifest"/>
                 <link rel="shortcut icon" href="/favicon-32x32.png"/>
-                <meta name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=no"
-                />
             </Head>
             <body>
                 <Main/>
